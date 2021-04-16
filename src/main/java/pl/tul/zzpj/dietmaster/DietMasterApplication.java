25c980e3174b66e96e912e83bb147bbda2c3d305
Bartłomiej Graczyk <224301@edu.p.lodz.pl>
2021-04-16 21:27:50 +0200

    Project initialization.

---
+++ src/main/java/pl/tul/zzpj/dietmaster/DietMasterApplication.java
@@ -0,0 +1,13 @@
+package pl.tul.zzpj.dietmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/tul/zzpj/dietmaster/DietMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tul.zzpj.dietmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
