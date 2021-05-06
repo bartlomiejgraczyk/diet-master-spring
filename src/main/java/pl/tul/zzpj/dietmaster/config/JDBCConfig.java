b5b50062df37c1079e3aaf504dbdd7499388645b
Bartłomiej Graczyk <224301@edu.p.lodz.pl>
2021-05-06 14:14:25 +0200

    Jenkins missing dataSource bean problem fix.

--- src/main/java/pl/tul/zzpj/dietmaster/config/JDBCConfig.java
+++ src/main/java/pl/tul/zzpj/dietmaster/config/JDBCConfig.java
@@ -1,6 +1,8 @@
 package pl.tul.zzpj.dietmaster.config;
 
 import javax.sql.DataSource;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,6 +23,7 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
         "pl.tul.zzpj.dietmaster.nutrient",
         "pl.tul.zzpj.dietmaster.registration.token"
 })
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class JDBCConfig extends AbstractJdbcConfiguration {
 
     private static final String PASSWORD = "d8eb9f7389777534fed3c6c6512e40912ff16dca60360bed2d2fc66dda697914";
