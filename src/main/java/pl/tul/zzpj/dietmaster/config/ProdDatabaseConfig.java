6cb967861fbb01274178923b7e31c9cc88ffec57
Bartłomiej Graczyk <224301@edu.p.lodz.pl>
2021-05-06 18:59:18 +0200

    Development H2 database added. Production and development properties files separated. H2 console enabled for development database.

---
+++ src/main/java/pl/tul/zzpj/dietmaster/config/ProdDatabaseConfig.java
@@ -0,0 +1,37 @@
+package pl.tul.zzpj.dietmaster.config;
+
+import javax.sql.DataSource;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+@Configuration
+@Profile("prod")
+public class ProdDatabaseConfig extends AbstractJdbcConfiguration {
+
+    private static final String USERNAME = "***REMOVED***";
+    private static final String PASSWORD = "***REMOVED***";
+    private static final String PREFIX = "jdbc:postgresql://";
+    private static final String HOST = "***REMOVED***";
+    private static final String PORT = "5432";
+    private static final String DB = "***REMOVED***";
+
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder<?> builder = DataSourceBuilder.create();
+        return builder
+                .driverClassName("org.postgresql.Driver")
+                .url(PREFIX + HOST + ":" + PORT + "/" + DB + "?user=" + USERNAME + "&password=" + PASSWORD)
+                .username(USERNAME)
+                .password(PASSWORD)
+                .build();
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate prodParameterJdbcTemplate() {
+        return new NamedParameterJdbcTemplate(this.dataSource());
+    }
+}
