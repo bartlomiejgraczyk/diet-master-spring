a2d38d7ba7e2e643eb4889ad1b4f7eac1f812d73
jakub_pietras <jacubeus@gmail.com>
2021-05-12 02:12:37 +0200

    Added scripts for initial data. Changed autogen.

--- src/main/java/pl/tul/zzpj/dietmaster/ingredient/IngredientCategory.java
+++ src/main/java/pl/tul/zzpj/dietmaster/ingredient/IngredientCategory.java
@@ -1,6 +1,26 @@
 package pl.tul.zzpj.dietmaster.ingredient;
 
 public enum IngredientCategory {
-    DAIRY, FRUIT, GRAIN, BEAN, NUT, FISH,
-    EGG, SWEET, MEAT, VEGETABLE, MUSHROOM, LIQUID
+    DAIRY("dairy"),
+    FRUIT("fruit"),
+    GRAIN("grain"),
+    BEAN("bean"),
+    NUT("nut"),
+    FISH("fish"),
+    EGG("egg"),
+    SWEET("sweet"),
+    MEAT("meat"),
+    VEGETABLE("vegetable"),
+    MUSHROOM("mushroom"),
+    LIQUID("liquid");
+
+    private final String name;
+
+    public String getName() {
+        return name;
+    }
+
+    IngredientCategory(String ingredientCategory) {
+        this.name = ingredientCategory;
+    }
 }
