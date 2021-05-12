6fcf2e872ca3bc16beec26fcaa1950a8da5d1184
jakub_pietras <jacubeus@gmail.com>
2021-05-12 02:12:37 +0200

    Added scripts for initial data. Changed autogen.

---
+++ database/ingredients.sql
@@ -0,0 +1,29 @@
+INSERT INTO ingredient (category, description, name)
+VALUES ('egg',
+        'Hard-boiled eggs are loaded with nutrients, protein and healthy fats.',
+        'Hard-boiled Egg'),
+
+       ('meat',
+        'Beef is primarily composed of protein and varying amounts of fat.',
+        'Beef'),
+
+
+       ('vegetable',
+        'Carrots’ water content ranges from 86–95%, and the edible portion consists of around 10% carbs. Carrots ' ||
+        'contain very little fat and protein.',
+        'Carrot'),
+
+
+       ('fruit',
+        'Pears come in many different varieties. Bartlett, Bosc, and D’Anjou pears are among the most popular, ' ||
+        'but around 100 types are grown worldwide.',
+        'Pear'),
+
+
+       ('dairy',
+        'The nutritional profile of milk is impressive. After all, it’s designed to fully nourish newborn animals.',
+        'Cow Milk');
+
+
+
+
---
+++ database/ingredients_nutritions.sql
@@ -0,0 +1,117 @@
+-- Hard-boiled Egg
+-- All per 100g
+INSERT INTO ingredient_nutrition (per100, ingredient, nutrient)
+VALUES (154, 1, 1),    -- Calories
+       (10, 1, 21),    -- Total Fat
+       (3, 1, 22),     -- Saturated Fat
+       (0, 1, 23),     -- Trans Fat
+       (0.37, 1, 24),  -- Cholesterol
+       (1.4, 1, 7),    -- Sodium
+       (0, 1, 2),      -- Total Carbohydrate
+       (0, 1, 3),      -- Dietary Fiber
+       (0, 1, 4),      -- Total Sugars
+       (0, 1, 5),      -- Added Sugars
+       (12, 1, 20),    -- Protein
+-- VITAMINS (in micro grams)
+       (2, 1, 36),     -- Vitamin D
+       (56, 1, 6),     -- Calcium
+       (2000, 1, 13),  -- Iron
+       (138000, 1, 8), -- Potassium
+       (400, 1, 30),   -- Riboflavin B2
+       (0.8, 1, 34),   -- Vitamin B12
+       (20, 1, 25),    -- Biotin
+       (2000, 1, 29),  -- Pantheonic Acid
+       (54, 1, 12),    -- Iodine
+       (2000, 1, 14),  -- Zinc
+       (30, 1, 18),    -- Selenium
+       (16, 1, 19),    -- Molybdenum
+       (294000, 1, 26);
+-- Choline
+
+-- Beef
+-- All per 100g
+INSERT INTO ingredient_nutrition (per100, ingredient, nutrient)
+VALUES (291, 2, 1),   -- Calories
+       (20, 2, 21),   -- Total Fat
+       (7.8, 2, 22),  -- Saturated Fat
+       (0, 2, 23),    -- Trans Fat
+       (0.87, 2, 24), -- Cholesterol
+       (0.63, 2, 7),  -- Sodium
+       (0, 2, 2),     -- Total Carbohydrate
+       (0, 2, 3),     -- Dietary Fiber
+       (0, 2, 4),     -- Total Sugars
+       (0, 2, 5),     -- Added Sugars
+       (26, 2, 20),   -- Protein
+-- VITAMINS (in micro grams)
+       (7000, 2, 6),  -- Calcium
+       (2700, 2, 13), -- Iron
+       (319000, 2, 8);
+-- Potassium
+
+
+-- Carrot
+-- All per 100g
+INSERT INTO ingredient_nutrition (per100, ingredient, nutrient)
+VALUES (41, 3, 1),     -- Calories
+       (0.24, 3, 21),  -- Total Fat
+       (0, 3, 22),     -- Saturated Fat
+       (0, 3, 23),     -- Trans Fat
+       (0, 3, 24),     -- Cholesterol
+       (0.69, 3, 7),   -- Sodium
+       (9.58, 3, 2),   -- Total Carbohydrate
+       (2.8, 3, 3),    -- Dietary Fiber
+       (4.54, 3, 4),   -- Total Sugars
+       (0, 3, 5),      -- Added Sugars
+       (0.93, 3, 20),  -- Protein
+-- VITAMINS (in micro grams)
+       (33000, 3, 6),  -- Calcium
+       (300, 3, 13),   -- Iron
+       (320000, 3, 8), -- Potassium
+       (841, 3, 32),   -- Vitamin A
+       (5900, 3, 35);
+-- Vitamin C
+
+
+-- Pear
+-- All per 100g
+INSERT INTO ingredient_nutrition (per100, ingredient, nutrient)
+VALUES (58, 4, 1),     -- Calories
+       (0.12, 4, 21),  -- Total Fat
+       (0.01, 4, 22),  -- Saturated Fat
+       (0, 4, 23),     -- Trans Fat
+       (0, 4, 24),     -- Cholesterol
+       (0.01, 4, 7),   -- Sodium
+       (15.46, 4, 2),  -- Total Carbohydrate
+       (3.1, 4, 3),    -- Dietary Fiber
+       (9.8, 4, 4),    -- Total Sugars
+       (0, 4, 5),      -- Added Sugars
+       (0.38, 4, 20),  -- Protein
+-- VITAMINS (in micro grams)
+       (9000, 4, 6),   -- Calcium
+       (170, 4, 13),   -- Iron
+       (238000, 4, 8), -- Potassium
+       (1, 4, 32),     -- Vitamin A
+       (8400, 4, 35); -- Vitamin C
+
+
+-- Cow Milk
+-- All per 100g
+INSERT INTO ingredient_nutrition (per100, ingredient, nutrient)
+VALUES (60, 5, 1),     -- Calories
+       (3.25, 5, 21),  -- Total Fat
+       (1.865, 5, 22),  -- Saturated Fat
+       (0, 5, 23),     -- Trans Fat
+       (0.01, 5, 24),     -- Cholesterol
+       (0.01, 5, 7),   -- Sodium
+       (4.52, 5, 2),  -- Total Carbohydrate
+       (0, 5, 3),    -- Dietary Fiber
+       (5.26, 5, 4),    -- Total Sugars
+       (0, 5, 5),      -- Added Sugars
+       (3.22, 5, 20),  -- Protein
+-- VITAMINS (in micro grams)
+       (113000, 5, 6),   -- Calcium
+       (30, 5, 13),   -- Iron
+       (143000, 5, 8), -- Potassium
+       (28, 5, 32),     -- Vitamin A
+       (0, 5, 35); -- Vitamin C
+
