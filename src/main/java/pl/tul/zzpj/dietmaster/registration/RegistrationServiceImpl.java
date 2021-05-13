6883b640aad652318a4778254f06510aa1ec42da
Bartłomiej Graczyk <224301@edu.p.lodz.pl>
2021-05-13 17:06:16 +0200

    SMTP server changed to GMAIL smtp. Activation email are now lead to main inbox.

--- src/main/java/pl/tul/zzpj/dietmaster/registration/RegistrationServiceImpl.java
+++ src/main/java/pl/tul/zzpj/dietmaster/registration/RegistrationServiceImpl.java
@@ -14,6 +14,7 @@ import pl.tul.zzpj.dietmaster.exception.AccountAlreadyConfirmedException;
 import pl.tul.zzpj.dietmaster.exception.ConfirmationTokenExpired;
 import pl.tul.zzpj.dietmaster.exception.EmailTakenException;
 import pl.tul.zzpj.dietmaster.exception.InvalidEmailException;
+import pl.tul.zzpj.dietmaster.exception.MailSendingException;
 import pl.tul.zzpj.dietmaster.exception.TokenNotFoundException;
 import pl.tul.zzpj.dietmaster.registration.token.ConfirmationToken;
 import pl.tul.zzpj.dietmaster.registration.token.ConfirmationTokenService;
@@ -29,25 +30,24 @@ public class RegistrationServiceImpl implements RegistrationService {
     private final EmailSender emailSender;
     private final PasswordEncoder passwordEncoder;
 
-    public void register(Account account) throws InvalidEmailException, EmailTakenException {
+    public void register(Account account, String url) throws InvalidEmailException, EmailTakenException, MailSendingException {
         boolean isValidEmail = emailValidator.
                 test(account.getEmail());
+
         if (!isValidEmail) {
             throw new InvalidEmailException(account.getEmail());
         }
-        
+
         account.getAccessLevels().add(new ClientAccessLevel());
-        
+
         String encodedPassword = passwordEncoder
                 .encode(account.getPassword());
         account.setPassword(encodedPassword);
-        
+
         Account createdAccount = accountService.addAccount(account);
 
-        String token = UUID.randomUUID().toString();
-
         ConfirmationToken confirmationToken = new ConfirmationToken(
-                token,
+                buildConfirmationToken(),
                 LocalDateTime.now(),
                 LocalDateTime.now().plusMinutes(15),
                 createdAccount
@@ -55,12 +55,9 @@ public class RegistrationServiceImpl implements RegistrationService {
 
         confirmationTokenService.saveConfirmationToken(confirmationToken);
 
-        String link = "http://localhost:8081/registration/confirm?token=" + token;
-        emailSender.send(
-                "noreply@dietmaster.com",
+        emailSender.sendActivationMail(
                 account.getEmail(),
-                "Confirm your account!",
-                buildEmail(account.getFirstName(), link)
+                buildConfirmationLink(url, confirmationToken.getToken())
         );
     }
 
@@ -85,72 +82,12 @@ public class RegistrationServiceImpl implements RegistrationService {
         accountService.enableAccount(confirmationToken.getAccount().getEmail());
     }
 
-    private String buildEmail(String name, String link) {
-        return "<div style=\"font-family:Helvetica,Arial,sans-serif;font-size:16px;margin:0;color:#0b0c0c\">\n" +
-                "\n" +
-                "<span style=\"display:none;font-size:1px;color:#fff;max-height:0\"></span>\n" +
-                "\n" +
-                "  <table role=\"presentation\" width=\"100%\" style=\"border-collapse:collapse;min-width:100%;width:100%!important\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\">\n" +
-                "    <tbody><tr>\n" +
-                "      <td width=\"100%\" height=\"53\" bgcolor=\"#0b0c0c\">\n" +
-                "        \n" +
-                "        <table role=\"presentation\" width=\"100%\" style=\"border-collapse:collapse;max-width:580px\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" align=\"center\">\n" +
-                "          <tbody><tr>\n" +
-                "            <td width=\"70\" bgcolor=\"#0b0c0c\" valign=\"middle\">\n" +
-                "                <table role=\"presentation\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse\">\n" +
-                "                  <tbody><tr>\n" +
-                "                    <td style=\"padding-left:10px\">\n" +
-                "                  \n" +
-                "                    </td>\n" +
-                "                    <td style=\"font-size:28px;line-height:1.315789474;Margin-top:4px;padding-left:10px\">\n" +
-                "                      <span style=\"font-family:Helvetica,Arial,sans-serif;font-weight:700;color:#ffffff;text-decoration:none;vertical-align:top;display:inline-block\">Confirm your email</span>\n" +
-                "                    </td>\n" +
-                "                  </tr>\n" +
-                "                </tbody></table>\n" +
-                "              </a>\n" +
-                "            </td>\n" +
-                "          </tr>\n" +
-                "        </tbody></table>\n" +
-                "        \n" +
-                "      </td>\n" +
-                "    </tr>\n" +
-                "  </tbody></table>\n" +
-                "  <table role=\"presentation\" class=\"m_-6186904992287805515content\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;max-width:580px;width:100%!important\" width=\"100%\">\n" +
-                "    <tbody><tr>\n" +
-                "      <td width=\"10\" height=\"10\" valign=\"middle\"></td>\n" +
-                "      <td>\n" +
-                "        \n" +
-                "                <table role=\"presentation\" width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse\">\n" +
-                "                  <tbody><tr>\n" +
-                "                    <td bgcolor=\"#1D70B8\" width=\"100%\" height=\"10\"></td>\n" +
-                "                  </tr>\n" +
-                "                </tbody></table>\n" +
-                "        \n" +
-                "      </td>\n" +
-                "      <td width=\"10\" valign=\"middle\" height=\"10\"></td>\n" +
-                "    </tr>\n" +
-                "  </tbody></table>\n" +
-                "\n" +
-                "\n" +
-                "\n" +
-                "  <table role=\"presentation\" class=\"m_-6186904992287805515content\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;max-width:580px;width:100%!important\" width=\"100%\">\n" +
-                "    <tbody><tr>\n" +
-                "      <td height=\"30\"><br></td>\n" +
-                "    </tr>\n" +
-                "    <tr>\n" +
-                "      <td width=\"10\" valign=\"middle\"><br></td>\n" +
-                "      <td style=\"font-family:Helvetica,Arial,sans-serif;font-size:19px;line-height:1.315789474;max-width:560px\">\n" +
-                "        \n" +
-                "            <p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\">Hi " + name + ",</p><p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\"> Thank you for registering. Please click on the below link to activate your account: </p><blockquote style=\"Margin:0 0 20px 0;border-left:10px solid #b1b4b6;padding:15px 0 0.1px 15px;font-size:19px;line-height:25px\"><p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\"> <a href=\"" + link + "\">Activate Now</a> </p></blockquote>\n Link will expire in 15 minutes. <p>See you soon</p>" +
-                "        \n" +
-                "      </td>\n" +
-                "      <td width=\"10\" valign=\"middle\"><br></td>\n" +
-                "    </tr>\n" +
-                "    <tr>\n" +
-                "      <td height=\"30\"><br></td>\n" +
-                "    </tr>\n" +
-                "  </tbody></table><div class=\"yj6qo\"></div><div class=\"adL\">\n" +
-                "\n" +
-                "</div></div>";
+    private String buildConfirmationLink(String url, String token) {
+        return url + "/confirm?token=" + token;
+
+    }
+
+    private String buildConfirmationToken() {
+        return UUID.randomUUID().toString();
     }
 }
