57f801fbc7a4b389ef7976aae9fc5fb505e7c181
pamsolik <amsithemlgpotato@gmail.com>
2021-05-16 19:15:52 +0200

    Some refactoring and added some exceptions to the addDiet method.

--- src/main/java/pl/tul/zzpj/dietmaster/diet/DietService.java
+++ src/main/java/pl/tul/zzpj/dietmaster/diet/DietService.java
@@ -1,6 +1,8 @@
 package pl.tul.zzpj.dietmaster.diet;
 
+import pl.tul.zzpj.dietmaster.exception.UserNotFoundException;
+
 public interface DietService {
-    void addDiet(CreateDietRequest diet);
+    void addDiet(CreateDietRequest diet) throws UserNotFoundException;
     void updateDiet(UpdateDietRequest diet);
 }
