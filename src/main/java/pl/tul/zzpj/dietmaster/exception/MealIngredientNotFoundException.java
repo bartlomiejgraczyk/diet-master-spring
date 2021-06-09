920f4a0f81e2df15f88617ccb6e7de06274e9af6
NorbertRudzki <norudzki@gmail.com>
2021-06-09 13:28:48 +0200

    Added update mealingredient operation

---
+++ src/main/java/pl/tul/zzpj/dietmaster/exception/MealIngredientNotFoundException.java
@@ -0,0 +1,15 @@
+package pl.tul.zzpj.dietmaster.exception;
+
+public class MealIngredientNotFoundException extends AppBaseException {
+
+    private static final String MEALINGREDIENT_NOT_FOUND = "MealIngredient with id %s was not found.";
+
+    public MealIngredientNotFoundException(Long id) {
+        super(String.format(MEALINGREDIENT_NOT_FOUND, id));
+    }
+
+    @Override
+    public String getCode() {
+        return super.getCode() + "_mealingredient_not_found";
+    }
+}
