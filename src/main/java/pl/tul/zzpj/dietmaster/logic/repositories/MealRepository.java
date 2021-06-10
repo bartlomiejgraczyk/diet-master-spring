e372586ce16e66cd8e59e4db02802a18c0304735
NorbertRudzki <norudzki@gmail.com>
2021-06-11 01:51:00 +0200

    Implemented GetMapping for finding meals by dietId

--- src/main/java/pl/tul/zzpj/dietmaster/logic/repositories/MealRepository.java
+++ src/main/java/pl/tul/zzpj/dietmaster/logic/repositories/MealRepository.java
@@ -2,8 +2,13 @@ package pl.tul.zzpj.dietmaster.logic.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import pl.tul.zzpj.dietmaster.model.entities.Diet;
 import pl.tul.zzpj.dietmaster.model.entities.Meal;
+
+import java.util.List;
 
 @Repository
 public interface MealRepository extends JpaRepository<Meal, Long> {
+    List<Meal> findMealsByContainingDiet_Id(long id);
+    Meal findMealById(long id);
 }
