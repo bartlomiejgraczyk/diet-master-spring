ab632af350602d935e7da0f388feae9ba6a21c89
jakub_pietras <jacubeus@gmail.com>
2021-06-11 01:20:38 +0200

    Finished tweaking NutrientController.

--- src/main/java/pl/tul/zzpj/dietmaster/logic/services/interfaces/NutrientService.java
+++ src/main/java/pl/tul/zzpj/dietmaster/logic/services/interfaces/NutrientService.java
@@ -1,17 +1,17 @@
 package pl.tul.zzpj.dietmaster.logic.services.interfaces;
 
 import pl.tul.zzpj.dietmaster.logic.controllers.requests.nutrient.CreateNutrientDto;
+import pl.tul.zzpj.dietmaster.logic.controllers.requests.nutrient.GetNutrientDto;
 import pl.tul.zzpj.dietmaster.logic.controllers.requests.nutrient.UpdateNutrientDto;
-import pl.tul.zzpj.dietmaster.model.entities.Nutrient;
 import pl.tul.zzpj.dietmaster.model.exception.exists.NutrientExistsException;
 
 import java.util.List;
 
 public interface NutrientService {
 
-    List<Nutrient> getAllNutrients();
+    List<GetNutrientDto> getAllNutrients();
 
-    List<Nutrient> getNutrientsOfCategory(String category);
+    List<GetNutrientDto> getNutrientsOfCategory(String category);
 
     void updateNutrient(UpdateNutrientDto updateNutrientDto) throws NutrientExistsException;
 
