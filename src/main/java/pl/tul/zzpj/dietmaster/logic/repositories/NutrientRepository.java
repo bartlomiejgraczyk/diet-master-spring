340296ba2c174d23e1825164c4d251ad8675ae34
jakub_pietras <jacubeus@gmail.com>
2021-06-11 00:32:35 +0200

    Implemented NutrientController. Small fixes. Added request files.

--- src/main/java/pl/tul/zzpj/dietmaster/logic/repositories/NutrientRepository.java
+++ src/main/java/pl/tul/zzpj/dietmaster/logic/repositories/NutrientRepository.java
@@ -3,7 +3,14 @@ package pl.tul.zzpj.dietmaster.logic.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.tul.zzpj.dietmaster.model.entities.Nutrient;
+import pl.tul.zzpj.dietmaster.model.entities.enums.categories.NutrientCategory;
+
+import java.util.List;
 
 @Repository
 public interface NutrientRepository extends JpaRepository<Nutrient, Long> {
+
+    List<Nutrient> findByCategory(NutrientCategory category);
+
+    Nutrient findDistinctByName(String name);
 }
