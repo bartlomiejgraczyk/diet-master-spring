298977aef5b0dfdf641c6a8ab57676e40b31d55a
NorbertRudzki <norudzki@gmail.com>
2021-06-11 01:51:00 +0200

    Implemented GetMapping for finding meals by dietId

--- src/main/java/pl/tul/zzpj/dietmaster/logic/services/implementations/MealServiceImpl.java
+++ src/main/java/pl/tul/zzpj/dietmaster/logic/services/implementations/MealServiceImpl.java
@@ -2,7 +2,9 @@ package pl.tul.zzpj.dietmaster.logic.services.implementations;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.tul.zzpj.dietmaster.logic.repositories.DietRepository;
 import pl.tul.zzpj.dietmaster.model.entities.Diet;
+import pl.tul.zzpj.dietmaster.model.exception.DietNotFoundException;
 import pl.tul.zzpj.dietmaster.model.exception.MealNotFoundException;
 import pl.tul.zzpj.dietmaster.model.entities.MealIngredient;
 import pl.tul.zzpj.dietmaster.logic.repositories.MealIngredientRepository;
@@ -12,19 +14,21 @@ import pl.tul.zzpj.dietmaster.logic.controllers.requests.meal.UpdateMealRequest;
 import pl.tul.zzpj.dietmaster.logic.repositories.MealRepository;
 import pl.tul.zzpj.dietmaster.logic.services.interfaces.MealService;
 
+import java.util.List;
 import java.util.Set;
 
 @Service
 @AllArgsConstructor
 public class MealServiceImpl implements MealService {
-    
-    private final MealRepository repository;
+
+    private final MealRepository mealRepository;
+    private final DietRepository dietRepository;
     private final MealIngredientRepository mealIngredientRepository;
     private final MealMapper mealMapper;
 
     @Override
     public void addMeal(Meal meal) {
-        repository.save(meal);
+        mealRepository.save(meal);
     }
 
     @Override
@@ -33,10 +37,10 @@ public class MealServiceImpl implements MealService {
         meals.toArray(mealsArray);
         for (Meal meal : mealsArray) {
             meal.setContainingDiet(diet);
-            repository.save(meal);
+            mealRepository.save(meal);
             MealIngredient[] mealIngredientsArray = new MealIngredient[meal.getMealIngredients().size()];
             meal.getMealIngredients().toArray(mealIngredientsArray);
-            for(MealIngredient mealIngredient : mealIngredientsArray){
+            for (MealIngredient mealIngredient : mealIngredientsArray) {
                 mealIngredient.setMeal(meal);
                 mealIngredientRepository.save(mealIngredient);
             }
@@ -44,13 +48,28 @@ public class MealServiceImpl implements MealService {
     }
 
     @Override
+    public List<Meal> getMealsByDietId(long id) throws DietNotFoundException {
+        if (!dietRepository.existsById(id))
+            throw new DietNotFoundException(id);
+        return mealRepository.findMealsByContainingDiet_Id(id);
+    }
+
+    @Override
+    public Meal getMealById(long id) throws MealNotFoundException {
+        if (!mealRepository.existsById(id))
+            throw new MealNotFoundException(id);
+        return mealRepository.findMealById(id);
+    }
+
+
+    @Override
     public void updateMeal(UpdateMealRequest dto) throws MealNotFoundException {
-        if(!repository.existsById(dto.getId()))
+        if (!mealRepository.existsById(dto.getId()))
             throw new MealNotFoundException(dto.getId());
-        Meal meal = repository.findById(dto.getId()).orElse(null);
+        Meal meal = mealRepository.findById(dto.getId()).orElse(null);
         mealMapper.updateMealFromDTO(dto, meal);
         assert meal != null;
-        repository.save(meal);
+        mealRepository.save(meal);
     }
 
 
