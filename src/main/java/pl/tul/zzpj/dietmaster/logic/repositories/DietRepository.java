e290b2c56904ab4c94749cb86e9a845dd7ef83b4
NorbertRudzki <norudzki@gmail.com>
2021-06-11 15:31:53 +0200

    Changed diet type from int to ENUM with converter

--- src/main/java/pl/tul/zzpj/dietmaster/logic/repositories/DietRepository.java
+++ src/main/java/pl/tul/zzpj/dietmaster/logic/repositories/DietRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.tul.zzpj.dietmaster.model.entities.Account;
 import pl.tul.zzpj.dietmaster.model.entities.Diet;
+import pl.tul.zzpj.dietmaster.model.entities.enums.types.DietType;
+
 import java.util.List;
 
 
@@ -22,6 +24,6 @@ public interface DietRepository extends JpaRepository<Diet, Long> {
             "IN (SELECT dc.dietitian FROM DietitianClient dc WHERE dc.client = ?1)")
     List<Diet> findAllMySubscribedDiets(Account user);
 
-    List<Diet> findDietsByType(int type);
+    List<Diet> findDietsByType(DietType type);
 }
 
---
+++ src/main/java/pl/tul/zzpj/dietmaster/model/entities/enums/types/TypeEnum.java
@@ -0,0 +1,5 @@
+package pl.tul.zzpj.dietmaster.model.entities.enums.types;
+
+public interface TypeEnum {
+    String getName();
+}
