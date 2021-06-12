207bf071345212b866aa225e5ad6c3626bfc76fd
pamsolik <amsithemlgpotato@gmail.com>
2021-06-12 21:56:30 +0200

    Implemented the MeasurementService tests.

--- src/main/java/pl/tul/zzpj/dietmaster/logic/services/interfaces/MeasurementService.java
+++ src/main/java/pl/tul/zzpj/dietmaster/logic/services/interfaces/MeasurementService.java
@@ -3,6 +3,7 @@ package pl.tul.zzpj.dietmaster.logic.services.interfaces;
 import pl.tul.zzpj.dietmaster.logic.controllers.requests.measurement.CreateMeasurementDto;
 import pl.tul.zzpj.dietmaster.logic.controllers.requests.measurement.GetMeasurementDto;
 import pl.tul.zzpj.dietmaster.logic.controllers.requests.measurement.UpdateMeasurementDto;
+import pl.tul.zzpj.dietmaster.model.entities.Measurement;
 import pl.tul.zzpj.dietmaster.model.exception.MeasurementSavedException;
 import pl.tul.zzpj.dietmaster.model.exception.notfound.UserNotFoundException;
 
@@ -12,11 +13,13 @@ import java.util.List;
 public interface MeasurementService {
     List<GetMeasurementDto> getAllMeasurements();
 
-    List<GetMeasurementDto> getMeasurementsOfClient(String user);
+    List<GetMeasurementDto> getMeasurementsOfClient(String user) throws UserNotFoundException;
 
     void updateMeasurement(UpdateMeasurementDto updateMeasurementDto) throws UserNotFoundException, MeasurementSavedException;
 
     void createMeasurement(CreateMeasurementDto createMeasurementDto) throws UserNotFoundException, MeasurementSavedException;
 
     void deleteMeasurement(Long id);
+
+    Measurement checkIfMeasurementExistsThenGet(Long id);
 }
