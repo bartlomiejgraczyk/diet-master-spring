5c458866f3986e1bbee0e15fa6f498a590b6518e
jakub_pietras <jacubeus@gmail.com>
2021-06-14 22:52:55 +0200

    Fixed errors. Added jwt to tests.

--- src/main/java/pl/tul/zzpj/dietmaster/logic/repositories/IngredientNutritionRepository.java
+++ src/main/java/pl/tul/zzpj/dietmaster/logic/repositories/IngredientNutritionRepository.java
@@ -2,6 +2,7 @@ package pl.tul.zzpj.dietmaster.logic.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import pl.tul.zzpj.dietmaster.model.entities.Ingredient;
 import pl.tul.zzpj.dietmaster.model.entities.IngredientNutrition;
 import pl.tul.zzpj.dietmaster.model.entities.Nutrient;
 
@@ -9,5 +10,9 @@ import pl.tul.zzpj.dietmaster.model.entities.Nutrient;
 public interface IngredientNutritionRepository extends JpaRepository<IngredientNutrition, Long> {
 
     boolean existsByNutrient(Nutrient nutrient);
+
+    boolean existsByNutrientAndIngredient(Nutrient nutrient, Ingredient ingredient);
+
+    void deleteByIngredient_Id(Long id);
 
 }
